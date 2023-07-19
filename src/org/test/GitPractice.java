49effd3bc7cb6a52df001d26b76f7b7f9cf0424a
KarthibanKN <karthibannatarajan@gmail.com>
2023-07-19 16:23:30 +0530

    New

--- src/org/test/GitPractice.java
+++ src/org/test/GitPractice.java
@@ -4,4 +4,8 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Source code transfer check - Eclipse to GitHUB");
 	}
+	private void merge2() {
+		System.out.println("New method for check conflict");
+
+	}
 }
